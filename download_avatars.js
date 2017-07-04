b7e481db50d0bc7eb2c7dd3e8dae41c71fa9ffec
Sidney Cardozo <scooby123sid@gmail.com>
2017-07-04 00:14:24 +0000

    added flexibility of using environment variables to set token

--- download_avatars.js
+++ download_avatars.js
@@ -2,9 +2,14 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = process.env.user;
+var GITHUB_TOKEN = process.env.token;
+
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
